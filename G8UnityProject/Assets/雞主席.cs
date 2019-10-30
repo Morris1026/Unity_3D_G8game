b5243c49bf8c41a4026666ad4ea80dd77d87cc35
unknown <871026m@gmail.com>
2019-10-30 16:04:20 +0800

    3D_G8GAME_2

---
+++ G8UnityProject/Assets/雞主席.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 雞主席 : MonoBehaviour
+{
+    int speed = 10;
+    float turn = 20.5f;
+    bool mission;
+    string name = "G8雞";
+
+}
