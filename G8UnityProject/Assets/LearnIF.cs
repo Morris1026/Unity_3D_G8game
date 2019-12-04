3fb412e711c85f191eb6fdc748769419e57bee28
Morris1026 <55180715+Morris1026@users.noreply.github.com>
2019-12-04 16:01:54 +0800

    3D_G8game_7

---
+++ G8UnityProject/Assets/LearnIF.cs
@@ -0,0 +1,42 @@
+﻿
+using UnityEngine;
+
+public class LearnIF : MonoBehaviour
+{
+    public bool openDoor;
+    public int score;
+
+    private void Start()
+    {
+        if(true)
+        {
+            print("測試!");
+        }
+    }
+
+
+   private void update()
+
+    {
+        if (openDoor)
+        {
+            print("開門");
+        }
+        else
+        {
+            print("關門");
+        }
+        if (score > 60)
+        {
+            print("及格");
+        }
+        else if (score >= 50) 
+        {
+            print("可以花錢補考ㄛ~~~");
+        }
+        else if (score >= 40) 
+        {
+            print("被當ㄌㄌㄌㄌ");
+        }
+    }
+}
