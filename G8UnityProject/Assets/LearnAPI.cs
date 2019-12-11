4dacb92899a509d049b38bca7f5332ac9790321d
Morris1026 <55180715+Morris1026@users.noreply.github.com>
2019-12-11 16:00:54 +0800

    3D_G8game_9

---
+++ G8UnityProject/Assets/LearnAPI.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class LearnPA : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Transform test1;
+    public Transform test2;
+
+    private void Start()
+    {
+        print(test1.position);
+
+        test1.localScale = new Vector3(10, 10, 10);
+
+        test2.localScale = Vector3.one * 5;
+
+    }
+
+    private void Update()
+    {
+        test1.Rotate(0, 5, 0);
+        test2.Translate(0, 0, 1.5f);
+    }
+}
--- G8UnityProject/Assets/雞主席.cs
+++ G8UnityProject/Assets/雞主席.cs
@@ -4,22 +4,41 @@ using UnityEngine;
 
 public class 雞主席 : MonoBehaviour
 {
-    [Header("移動速度")][Range(1,100)]
+    [Header("移動速度")][Range(1,2500)]
     public int speed = 10;
     [Tooltip("雞主席的旋轉速度"),Range(1.5f  ,200f)]
     public float turn = 20.5f;
     public bool mission;
-    public string name = "雞主席";
+    public string _name = "雞主席";
 
+    public Transform tran;
+    public Rigidbody rig;
+    public Animation ani;
+
+
+    private void Update()
+    {
+        Turn();
+        Run();
+        Bark();
+        Catch();
+        Task();
+    }
 
     private void Run()
     {
-
+        float V = Input.GetAxis("Vertical");   //W上1  S下-1  沒按=0
+        //rig.Addforce(0,0,speed*v);    //世界座標
+        //tran.forword  區域座標Z軸
+        //tran.right    區域座標X軸
+        //tran.up       區域座標Y軸
+        rig.AddForce(tran.forward * speed * V * Time.deltaTime);
     }
 
     private void Turn()
     {
-
+        float h = Input.GetAxis("Horizontal");   //A=左 -1  D=右 1  沒按=0  
+        tran.Rotate(0, turn * h * Time.deltaTime, 0);
     }
 
     private void Bark()
